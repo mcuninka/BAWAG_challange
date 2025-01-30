55ef34b0c2966a7bb33780240b754cbf242a406b
Mareno <maros.cuninka@gmail.com>
2025-01-31 00:41:24 +0100

    Finished 4 charts + readme

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # BAWAG_challange
-Streamlit application to analyze NHL data
+
+Streamlit application to analyze online retail stores data
+It shows:
+
+- Data preview
+- Total views & purchases of different categories by site
+
+You can filter by site or choose various categories (multiple selection available)
+
+---
+
+To run the application locally, you'll need:
+
+1. pandas
+2. plotly express
+3. streamlit
+
+You can run this code to install them all: `pip install pandas plotly-express streamlit`
+
+Then run `streamlit run main.py` to open the app in the browser.
---
+++ main.py
@@ -0,0 +1,196 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+st.set_page_config(layout="wide")
+
+
+@st.cache_data
+def load_data():
+    df = pd.read_csv("Retail sites.csv")
+    df["date"] = pd.to_datetime(
+        "20" + df["YEAR"].astype(str) + "-" + df["MONTH"].astype(str) + "-01"
+    )
+    return df
+
+
+data = load_data()
+
+if data is not None:
+    st.title("Retail Data")
+    st.subheader("Data Preview")
+    st.write(data.head())
+
+    st.subheader("Total Purchases by Site")
+
+    col1, col2, col3 = st.columns(3)
+
+    grouped_data = data.groupby("SITE")["TOTAL_PURCHASES"].sum().reset_index()
+
+    fig = px.pie(
+        grouped_data,
+        values="TOTAL_PURCHASES",
+        names="SITE",
+        height=300,
+        width=200,
+    )
+    fig.update_layout(
+        margin=dict(l=0, r=0, t=50, b=0),
+    )
+    col1.plotly_chart(fig, use_container_width=True)
+
+    # Filter data
+    st.subheader("Filter Data")
+
+    selected_site = st.pills(
+        "Site", data["SITE"].unique().tolist(), default="amazon.com"
+    )
+
+    # Filter by category
+    categories = data["MAIN_CATEGORY"].unique().tolist()
+    selected_categories = st.multiselect("Select category", categories)
+
+    # Display metric data and chart
+    col1, col2 = st.columns(2)
+
+    total_views = round(data["TOTAL_VIEWS"].sum() / 1_000_000, 2)
+    total_purchases = round(data["TOTAL_PURCHASES"].sum() / 1_000_000, 2)
+    chart_data = (
+        data[["date", "TOTAL_VIEWS", "TOTAL_PURCHASES"]]
+        .groupby("date")
+        .sum()
+        .reset_index()
+    )
+
+    top10 = (
+        data[["MAIN_CATEGORY", "TOTAL_PURCHASES"]]
+        .groupby("MAIN_CATEGORY")
+        .sum()
+        .sort_values("TOTAL_PURCHASES", ascending=False)
+        .head(10)
+        .reset_index()
+    )
+
+    if selected_site is not None:
+        if len(selected_categories) > 0:
+            total_views = round(
+                data.loc[
+                    (data["SITE"] == selected_site)
+                    & (data["MAIN_CATEGORY"].isin(selected_categories)),
+                    "TOTAL_VIEWS",
+                ].sum()
+                / 1_000_000,
+                2,
+            )
+            total_purchases = round(
+                data.loc[
+                    (data["SITE"] == selected_site)
+                    & (data["MAIN_CATEGORY"].isin(selected_categories)),
+                    "TOTAL_PURCHASES",
+                ].sum()
+                / 1_000_000,
+                2,
+            )
+
+            chart_data = (
+                data.loc[
+                    (data["SITE"] == selected_site)
+                    & (data["MAIN_CATEGORY"].isin(selected_categories)),
+                    ["date", "TOTAL_VIEWS", "TOTAL_PURCHASES"],
+                ]
+                .groupby("date")
+                .sum()
+                .reset_index()
+            )
+
+        else:
+            total_views = round(
+                data.loc[data["SITE"] == selected_site, "TOTAL_VIEWS"].sum()
+                / 1_000_000,
+                2,
+            )
+            total_purchases = round(
+                data.loc[data["SITE"] == selected_site, "TOTAL_PURCHASES"].sum()
+                / 1_000_000,
+                2,
+            )
+
+            chart_data = (
+                data.loc[
+                    data["SITE"] == selected_site,
+                    ["date", "TOTAL_VIEWS", "TOTAL_PURCHASES"],
+                ]
+                .groupby("date")
+                .sum()
+                .reset_index()
+            )
+
+        top10 = (
+            data.loc[
+                data["SITE"] == selected_site, ["MAIN_CATEGORY", "TOTAL_PURCHASES"]
+            ]
+            .groupby("MAIN_CATEGORY")
+            .sum()
+            .sort_values("TOTAL_PURCHASES", ascending=False)
+            .head(10)
+            .reset_index()
+        )
+
+    else:
+        if len(selected_categories) > 0:
+            total_views = round(
+                data.loc[
+                    data["MAIN_CATEGORY"].isin(selected_categories),
+                    "TOTAL_VIEWS",
+                ].sum()
+                / 1_000_000,
+                2,
+            )
+
+            total_purchases = round(
+                data.loc[
+                    data["MAIN_CATEGORY"].isin(selected_categories),
+                    "TOTAL_PURCHASES",
+                ].sum()
+                / 1_000_000,
+                2,
+            )
+
+            chart_data = (
+                data.loc[
+                    data["MAIN_CATEGORY"].isin(selected_categories),
+                    ["date", "TOTAL_VIEWS", "TOTAL_PURCHASES"],
+                ]
+                .groupby("date")
+                .sum()
+                .reset_index()
+            )
+
+    col1.metric("Total Views", total_views)
+    col2.metric("Total Purchase", total_purchases)
+
+    col1.write("Total Views and Purchases")
+    if chart_data.empty:
+        col1.write("Selected Site has not sold any of the selected categories")
+    else:
+        col1.line_chart(
+            chart_data,
+            x="date",
+            y=["TOTAL_VIEWS", "TOTAL_PURCHASES"],
+            color=["#B7FFFA", "#FFC067"],
+            height=400,
+        )
+
+    col2.write("Top 10 Most Purchased Categories")
+    col2.bar_chart(
+        top10,
+        x="MAIN_CATEGORY",
+        y="TOTAL_PURCHASES",
+        horizontal=True,
+        height=400,
+        color="#B7FFFA",
+    )
+
+
+else:
+    st.write("No data to display")
